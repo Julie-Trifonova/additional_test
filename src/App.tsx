3423b750bd27b04c502463b6e46baa6ca74f624b
Juliya <yula_sakura@list.ru>
2023-08-03 17:43:18 +0300

    Add_changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,70 @@
 import React, {useEffect, useRef, useState} from 'react';
 
 function App() {
+
     const Ref = useRef(null);
+    const [isRunning, setIsRunning] = useState(false)
     const [timer, setTimer] = useState('00:00:00');
+    const [currentTotalSeconds, setCurrentTotalSeconds] = useState(0);
+    const createTimerAnimator = (totalSeconds: any) => {
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
 
+        if (totalSeconds > 0) {
+            setTimer(
+                (hours > 9 ? hours : '0' + hours) + ':' +
+                (minutes > 9 ? minutes : '0' + minutes) + ':'
+                + (seconds > 9 ? seconds : '0' + seconds)
+            )
+        }
+    }
 
-    // const clearTimer = (e: any) => {
-    //     setTimer(seconds);
-    //     if (Ref.current) clearInterval(Ref.current);
-    //     const id = setInterval(() => {
-    //         startTimer(e);
-    //     }, 1000)
-    //     Ref.current = id;
-    // }
-    // const getDeadTime = () => {
-    //     let deadline = new Date();
-    //     deadline.setSeconds(deadline.getSeconds() + seconds);
-    //     return deadline;
-    // }
-    // useEffect(() => {
-    //     clearTimer(getDeadTime());
-    // }, []);
-    // const onClickReset = () => {
-    //     clearTimer(getDeadTime());
-    // }
-
-    const createTimerAnimator = () => {
-        return (seconds: any) => {};
-    };
-    const animateTimer = createTimerAnimator();
     const onChangeInput = (e: any) => {
         const totalSeconds = e.target.value.replace(/\D/g, '');
-        if(totalSeconds < 86400) {
+        if (totalSeconds < 86400) {
             e.target.value = totalSeconds;
-            const hours = Math.floor(totalSeconds / 3600);
-            const minutes = Math.floor((totalSeconds % 3600) / 60);
-            const seconds = totalSeconds % 60;
-
-            if (totalSeconds > 0) {
-                        setTimer(
-                            (hours > 9 ? hours : '0' + hours) + ':' +
-                            (minutes > 9 ? minutes : '0' + minutes) + ':'
-                            + (seconds > 9 ? seconds : '0' + seconds)
-                        )
-                    }
-            // const date = new Date(seconds * 1000).toISOString().slice(11, 19);
+            setCurrentTotalSeconds(totalSeconds);
+            createTimerAnimator(totalSeconds)
         } else {
             e.target.value = 'maximum value is 86399';
         }
-
-        // animateTimer(seconds);
-        // e.preventDefault()
     }
 
-    const onClickButton = (e: any) => {
-        e.target.value = '';
-    }
+    useEffect(() => {
+        if (isRunning) {
+            const interval = setInterval(() => {
+                const newTotalSeconds = currentTotalSeconds - 1;
+                createTimerAnimator(newTotalSeconds);
+                setCurrentTotalSeconds(newTotalSeconds);
+                console.log(newTotalSeconds)
+            }, 1000)
+            if (currentTotalSeconds === 0) {
+                setIsRunning(false)
+                setTimer('00:00:00')
+                clearInterval(interval)
+            }
+            // return () => clearInterval(interval)
+        }
+    }, [isRunning, timer])
 
-  return (
-    <div className="App">
-        <input onChange={(e) => onChangeInput(e)}
-               placeholder="Seconds"
-               type="text"
-        />
-        {/*<button onClick={(e) => onClickButton(e)}>Start</button>*/}
-        {/*<button onClick={onClickReset}>Start</button>*/}
-        <br />
-        <br />
-        <span>{timer}</span>
-    </div>
-  );
+    // useEffect(() => {
+    //     setResetTime(timer)
+    // }, [])
+
+    return (
+        <div className="App">
+            <input onChange={(e) => onChangeInput(e)}
+                   placeholder="Seconds"
+                   type="text"
+            />
+            <button onClick={() => setIsRunning(true)}>Start</button>
+            <br/>
+            <br/>
+            <span>{timer}</span>
+        </div>
+    );
 }
 
 export default App;
-
-
 
