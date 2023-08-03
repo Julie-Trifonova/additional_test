62b891721444911b90ff6ac93a29f5c27606859d
Juliya <yula_sakura@list.ru>
2023-08-03 15:33:06 +0300

    Add_project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
 
 function App() {
+    const Ref = useRef(null);
+    const [timer, setTimer] = useState('00:00:00');
+
+
+    // const clearTimer = (e: any) => {
+    //     setTimer(seconds);
+    //     if (Ref.current) clearInterval(Ref.current);
+    //     const id = setInterval(() => {
+    //         startTimer(e);
+    //     }, 1000)
+    //     Ref.current = id;
+    // }
+    // const getDeadTime = () => {
+    //     let deadline = new Date();
+    //     deadline.setSeconds(deadline.getSeconds() + seconds);
+    //     return deadline;
+    // }
+    // useEffect(() => {
+    //     clearTimer(getDeadTime());
+    // }, []);
+    // const onClickReset = () => {
+    //     clearTimer(getDeadTime());
+    // }
+
+    const createTimerAnimator = () => {
+        return (seconds: any) => {};
+    };
+    const animateTimer = createTimerAnimator();
+    const onChangeInput = (e: any) => {
+        const totalSeconds = e.target.value.replace(/\D/g, '');
+        if(totalSeconds < 86400) {
+            e.target.value = totalSeconds;
+            const hours = Math.floor(totalSeconds / 3600);
+            const minutes = Math.floor((totalSeconds % 3600) / 60);
+            const seconds = totalSeconds % 60;
+
+            if (totalSeconds > 0) {
+                        setTimer(
+                            (hours > 9 ? hours : '0' + hours) + ':' +
+                            (minutes > 9 ? minutes : '0' + minutes) + ':'
+                            + (seconds > 9 ? seconds : '0' + seconds)
+                        )
+                    }
+            // const date = new Date(seconds * 1000).toISOString().slice(11, 19);
+        } else {
+            e.target.value = 'maximum value is 86399';
+        }
+
+        // animateTimer(seconds);
+        // e.preventDefault()
+    }
+
+    const onClickButton = (e: any) => {
+        e.target.value = '';
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input onChange={(e) => onChangeInput(e)}
+               placeholder="Seconds"
+               type="text"
+        />
+        {/*<button onClick={(e) => onClickButton(e)}>Start</button>*/}
+        {/*<button onClick={onClickReset}>Start</button>*/}
+        <br />
+        <br />
+        <span>{timer}</span>
     </div>
   );
 }
 
 export default App;
+
+
+
